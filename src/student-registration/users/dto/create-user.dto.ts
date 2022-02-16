0e31f9e7b8b058c874a154d636a71e997d800b8b
keneumeh <73092002+keneumeh@users.noreply.github.com>
2022-02-16 18:34:12 +0100

    page 226 nestjs primer

--- src/student-registration/users/dto/create-user.dto.ts
+++ src/student-registration/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+    
+    readonly firstName: string;
+    readonly middleName?: string;
+    readonly lastName: string;
+    readonly email: string;
+    readonly dateOfBirth: Date;
+    readonly nationality?: string
+    readonly address?: string
+    readonly isActive?: boolean;
+    
+}
--- src/student-registration/users/users.controller.ts
+++ src/student-registration/users/users.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, Render} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+//these are requests from the client side
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Get('create')
+  @Render('users/create-user.html')
+  createForm() {
+  }
+  
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto) { //Dto = data transfer object
     return this.usersService.create(createUserDto);
   }
 
@@ -22,7 +27,7 @@ export class UsersController {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
@@ -31,4 +36,5 @@ export class UsersController {
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
+  
 }
